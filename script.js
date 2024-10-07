da6e66772128d1f4f0616895897b8f27d1a021a6
pedro-chandoha <160602791+pedro-chandoha@users.noreply.github.com>
2024-10-07 11:47:27 -0300

    base

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-function somaDeValoresDoArray() {
-    function somaArray(arr) {
-        var soma;
-        for (var num of arr) {
-            soma += num;
-        }
-        return soma;
+function calculoSoma() {
+    let n = parseInt(prompt("Quantos valores serão digitados ? "));
+    let arr = [];
+    let i = 0;
+    while (i < n) {
+      let value = parseFloat(prompt(`Insira o valor ${i + 1}: `));
+      arr.push(value);
+      i++;
     }
-    var arr = [];
-    var quantNum = prompt("Digite quantos numeros serão digitados: ");
-    for (let i = 0; i < quantNum; i++) {
-        arr[i] = prompt("Digite o valor: ")
-    }
-    var resultado = somaArray(arr);
-    console.log(resultado);
-}
+    let sum = arr.reduce((total, currentValue) => total + currentValue, 0);
+    alert(`A soma dos valores do array é: ${sum}`);
+  }
